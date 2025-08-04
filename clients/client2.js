c94305e45f6a38653c81503ab82570dcd8e731a5
Mukagal1 <93695446+Mukagal1@users.noreply.github.com>
2025-08-04 21:06:18 +0500

    Update client2.js

--- clients/client2.js
+++ clients/client2.js
@@ -3,7 +3,10 @@ const { create } = require('@wppconnect-team/wppconnect');
 async function initClient(sessionName) {
   const client = await create({
     session: sessionName,
-    puppeteerOptions: { headless: true },
+    puppeteerOptions: {
+      headless: true,
+      args: ['--no-sandbox', '--disable-setuid-sandbox']
+    },
   });
   return client;
 }
